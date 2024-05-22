f4b038db84c3ba120c29c48fcc552be12e5ac2c9
Matt Bryant <brybrant@gmail.com>
2024-05-22 01:12:51 +0100

    Single quote supremacy

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,16 @@
-import js from "@eslint/js";
-import eslintPluginSolid from "eslint-plugin-solid/configs/recommended.js";
-import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
+import js from '@eslint/js';
+import eslintPluginSolid from 'eslint-plugin-solid/configs/recommended.js';
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
 
 export default [
   js.configs.recommended,
   eslintPluginSolid,
   eslintPluginPrettier,
   {
-    "prettier/prettier": [
+    'prettier/prettier': [
       {
-        "endOfLine": "auto",
+        'endOfLine': 'auto',
       },
-    ]
+    ],
   },
 ];
